5e612c685b02ab4c3d5278a00c0eb058379f712e
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-19 10:52:17 +0100

    Demo Async en Await keywords bij asynchroon programmeren

---
+++ DemoAsyncAwait/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DemoAsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string result = await Method1();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private static async Task<string> Method1()
+        {
+            string boodschap = "Mijn asynchrone methode 1 roept andere asynchrone methode Werkuren aan: ";
+            var result = await WerkUren();
+            return boodschap + result + " werkuren vandaag";
+        }
+
+        private static async Task<int> WerkUren()
+        {
+            DayOfWeek today = await Task.FromResult<DayOfWeek>(DateTime.Now.DayOfWeek);
+            if (today == DayOfWeek.Saturday || today == DayOfWeek.Sunday)
+            {
+                return 0;
+            }
+            else
+            {
+                return 8;
+            }
+        }
+    }
+}
