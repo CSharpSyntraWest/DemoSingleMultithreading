60d0be8aa3fc942b4d13a304fa27f4d2b54411ca
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-19 10:03:10 +0100

    Demo Task Return value

---
+++ DemoTaskReturnValue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DemoTaskReturnValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoofd-thread opgestart");
+            Task<double> task1 = Task.Run(() =>
+            {
+                return BerekenSom(10);
+            });
+            Console.WriteLine("De som van de eerste 10 getallen is: " + task1.Result);
+            Console.WriteLine("Hoofd-thread beëindigd");
+            Console.ReadKey();
+        }
+
+        private static double BerekenSom(int getal)
+        {
+            double som = 0;
+            for (int teller = 1; teller <= getal; teller++)
+            {
+                som += teller;  
+            }
+            return som;
+        }
+    }
+}
