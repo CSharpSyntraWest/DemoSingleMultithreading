b25ec3d618c258baf78e4e8bd989df3ea1c06120
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-19 10:18:02 +0100

    Demo 1 Tasks Chaining dmv ContinueWith

---
+++ DemoTaskContinueWith/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DemoTaskContinueWith
+{
+    class Program
+    {
+        //Demo Chaining van Tasks dmv ContinueWith
+        static void Main(string[] args)
+        {
+            Task<string> task1 = Task.Run(()=>
+            {
+                return 12;
+            }).ContinueWith( (result)=>
+                 {
+                     return "Kwadraat van " + result.Result + " is : " + result.Result * result.Result;
+                 }            
+            );
+            Console.WriteLine(task1.Result);
+            Console.ReadKey();
+        }
+    }
+}
